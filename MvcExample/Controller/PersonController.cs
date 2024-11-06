3830e205c556e54c064ba2f7b2e702813dbb32e3
StanB <stanb791@gmail.com>
2024-11-06 08:10:27 +0300

    Добавьте файлы проекта.

---
+++ MvcExample/Controller/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using MvcExample.Model;
+using MvcExample.View;
+using System.Windows.Forms;
+namespace MvcExample.Controller
+{
+    public class PersonController
+    {
+        private MainForm view;
+        public PersonController(MainForm view)
+        {
+            this.view = view;
+            this.view.AddPerson += OnAddPerson;
+        }
+        private void OnAddPerson(object sender, System.EventArgs e)
+        {
+            string name = view.Names;
+            int age = view.Age;
+            Person person = new Person { Name = name, Age = age };
+            view.DisplayResult($"Добавлено: {person.Name}, Возраст: {person.Age}");
+        }
+    }
+}
---
+++ MvcExample/View/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using MvcExample.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MvcExample.View
+{
+    public partial class MainForm : Form
+    {
+        public event EventHandler AddPerson;
+        private PersonController controller;
+        public string Names => textBox1.Text;
+        public int Age => int.TryParse(textBox2.Text, out int age) ? age : 0;
+
+        public MainForm()
+        {
+            InitializeComponent(GetName());
+            controller = new PersonController(this);
+        }
+
+        public void DisplayResult(string result)
+        {
+            label1.Text = result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddPerson?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
